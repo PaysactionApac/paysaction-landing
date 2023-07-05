b7cef81762bc371fe5e42a2329ab6365088fbbaf
Yermira Makhatova <yermiramakhatova@Yermiras-MacBook-Air.local>
2023-07-05 21:28:54 +0600

    Edit the layout

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import { Button } from "../Button";
 import * as styles from "./Form.module.scss";
+import OutlinedLogo from "../../images/outlined-logo.svg";
 
 export const Form = () => {
     const [company, setCompany] = useState('');
     const [email, setEmail] = useState('');
+    const [dataSent, setDataSent] = useState(false);
 
     const handleSubmit = () => {
         if (company && email) {
-            console.info(company, email);
+            setDataSent(true);
+            setEmail('');
+            setCompany('');
         }
+    };
+
+    const handleOK = () => {
+        setDataSent(false);
     };
 
     const handleCompanyChange = (e) => {
@@ -23,34 +31,45 @@ export const Form = () => {
 
     return (
         <section className={styles.root} id="formSection">
-            <StaticImage
-                src="../../images/message.png"
-                loading="lazy"
-                alt="message"
-                className={styles.img}
-            />
-
-            <div className={styles.form}>
-                <h2>
-                    Start collecting your payments from your customers automatically from the first day
-                </h2>
-                <input
-                    type="text"
-                    className={styles.input}
-                    value={company}
-                    onChange={handleCompanyChange}
-                    placeholder="Enter Company name"
-                />
-                <input
-                    type="email"
-                    className={styles.input}
-                    value={email}
-                    onChange={handleEmailChange}
-                    placeholder="Enter your e-mail"
+            {!dataSent ?
+            <>
+                <StaticImage
+                    src="../../images/message.png"
+                    loading="lazy"
+                    alt="message"
+                    className={styles.img}
+                    objectFit="contain"
                 />
 
-                <Button onClick={handleSubmit}>Get started</Button>
-            </div>
+                <div className={styles.form}>
+                    <h2>
+                        Start collecting your payments from your customers automatically from the first day
+                    </h2>
+                    <input
+                        type="text"
+                        className={styles.input}
+                        value={company}
+                        onChange={handleCompanyChange}
+                        placeholder="Enter Company name"
+                    />
+                    <input
+                        type="email"
+                        className={styles.input}
+                        value={email}
+                        onChange={handleEmailChange}
+                        placeholder="Enter your e-mail"
+                    />
+
+                    <Button onClick={handleSubmit}>Get started</Button>
+                </div>
+            </> : (
+                <div className={styles.thanks}>
+                    <img src={OutlinedLogo} alt="logo" />
+
+                    <h2>Thank you! We will be in touch shortly</h2>
+                    <Button onClick={handleOK}>OK</Button>
+                </div>
+            )}
         </section>
     );
 };
